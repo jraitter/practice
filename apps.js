ac55e9f6fd71bf6a685c6660643792a8f8cfbd5e
jraitter <jraitter@cableone.net>
2019-12-13 21:35:53 -0700

    First commit of practice files

---
+++ apps.js
@@ -0,0 +1,20 @@
+console.log("hellow big world");
+let myName = "James";
+let x = 10;
+
+let james = {
+  name: "Big James"
+};
+
+console.log(myName);
+console.log(james.name);
+//console.log(document);
+
+document.getElementById("name").innerText = james.name;
+let me = {
+  name: "James",
+  age: 22,
+  eyeColor: "brown"
+}
+
+document.write("where will this get put?");
